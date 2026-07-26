89c18cec0551c377a440ade8f10002c735ed515b
Sara <s.cristal.p@gmail.com>
2026-07-26 18:13:51 +0200

    rm extra import

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,5 @@
 import db from "#db/client";
 import bcrypt from "bcrypt"; //remember to encrypt our passwords
-import db from "#db/client";
 
 await db.connect(); //opens the database connection.
 await seed(); //runs the function where we will insert the starter data.
@@ -114,49 +113,40 @@ const productData = [
     ["2026-07-26", "Home gym essentials", user.id],
   );
 
-//User who has made at least 1 order of at least 5 distinct products.
-const {
-  rows: [order],
-} = await db.query(
-  `
-    INSERT INTO orders (date, note, user_id)
-    VALUES ($1, $2, $3)
-    RETURNING *;
-  `,
-  ["2026-07-26", "Home gym essentials", user.id],
-);
+ // 5. Select five distinct products for the order
+  const orderProductData = [
+    {
+      productId: createdProducts[0].id,
+      quantity: 2,
+    },
+    {
+      productId: createdProducts[1].id,
+      quantity: 1,
+    },
+    {
+      productId: createdProducts[2].id,
+      quantity: 1,
+    },
+    {
+      productId: createdProducts[3].id,
+      quantity: 2,
+    },
+    {
+      productId: createdProducts[4].id,
+      quantity: 1,
+    },
+  ];
 
-const orderProductData = [
-  {
-    productId: createdProducts[0].id,
-    quantity: 2,
-  },
-  {
-    productId: createdProducts[1].id,
-    quantity: 1,
-  },
-  {
-    productId: createdProducts[2].id,
-    quantity: 1,
-  },
-  {
-    productId: createdProducts[3].id,
-    quantity: 2,
-  },
-  {
-    productId: createdProducts[4].id,
-    quantity: 1,
-  },
-];
-
-for (const orderProduct of orderProductData) {
-  await db.query(
-    `
-      INSERT INTO orders_products (order_id, product_id, quantity)
-      VALUES ($1, $2, $3);
-    `,
-    [order.id, orderProduct.productId, orderProduct.quantity],
-  );
+// 6. Connect the order and products through the junction table
+  for (const orderProduct of orderProductData) {
+    await db.query(
+      `
+        INSERT INTO orders_products (order_id, product_id, quantity)
+        VALUES ($1, $2, $3);
+      `,
+      [order.id, orderProduct.productId, orderProduct.quantity],
+    );
+  }
 }
 
 
